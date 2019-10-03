476d8c71ab7765150e6b85452ab459bcd1371784
github username <github email address>
2019-10-03 13:58:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+
+    get countSides(){
+        return this.array.length
+    }
+
+    get perimeter(){
+        let side = this.array.split
+    }
+}
+
+class Triangle extends Polygon{
+
+}
+
+class Square extends Polygon{
+    
+}
